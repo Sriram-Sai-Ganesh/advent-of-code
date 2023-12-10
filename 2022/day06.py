e5bb090e45b5e7f4f2691a22a1f6d4b711751e63
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2023-12-09 21:54:18 -0500

    sol

--- 2022/day06.py
+++ 2022/day06.py
@@ -1,76 +1,26 @@
-# https://adventofcode.com/2022/day/6
+# https://adventofcode.com/2023/day/6
 
 TEST=0
 
 file='input/'+('test.txt' if TEST else 'in.txt')
 
-from re import *
-from math import *
-from collections import *
-import numpy as np
-import itertools
-
 # all lines from file
 def lines():
     with open(file) as f:
         return [x.strip('\n') for x in f.readlines()]
-    
-# ignore everything except numbers, return a list of nums
-def nums(line):
-    return list(map(int,sub('\D+',' ',line).strip().split()))
-
-
-# empty-line-separated groups of lines
-def groups_of_lines():
-    result=[]
-    i=0
-    li=lines()
-    while i<len(li):
-        group=[]
-        while i<len(li) and li[i]!='':
-            group.append(li[i])
-            i+=1
-        result.append(group)
-        i+=1
-    return result
-
-# empty-line-separated groups of integers
-def groups_of_ints():
-    return [list(map(int, row)) for row in groups_of_lines()]
-
-# list of 1 int on each line
-def ints():
-    return list(map(int, lines()))
-
-# 2D list of strings or characters, separated by @param separ.
-def line_grid(separ=''):
-    return [x.split(separ) for x in lines()]
-
-# 2D list of ints, separated by @param separ.
-def int_grid(separ=''):
-    return [list(map(int, row)) for row in line_grid(separ)]
-
-
-"""
-lines(): 
-    all lines from file
-nums(line):
-    ignore everything except numbers, return a list of nums
-groups_of_lines():
-    empty-line-separated groups of lines
-string_grid(separ=''):
-    2D list of strings or characters, separated by @param separ.
-groups_of_ints():
-    empty-line-separated groups of integers
-ints():
-    list of 1 int on each line
-int_grid(separ=''):
-    2D list of ints, separated by @param separ.
-"""
 
 p=print
 
 all = lines()
+line=all[0]
 
+def solve(line, doPart2=False):
+    # only difference between parts 1 and 2 is # of chars to look ahead
+    num=4 if not doPart2 else 14
+    for i in range(len(line)):
+        # size of (set of chars) should be 4 (or 14 for part 2)
+        if len(set(line[i:i+num]))==num:
+            return i+num
 
-
+p('Part 1:',solve(line, False))
+p('Part 2:',solve(line, True))
